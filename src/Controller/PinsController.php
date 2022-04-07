7a07abd3cef83a1337604534e28d784125e46695
dorian <dorian.fenestre@free.fr>
2022-04-07 14:14:52 +0200

    added a delete feature. JS workaround to be able to use delete without showing submit button - see in show.html.twig

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -67,7 +67,7 @@ class PinsController extends AbstractController
         $form->handleRequest($request);
 
         if($form->isSubmitted() && $form->isValid()) {
-           
+
             $em->flush();
 
             return $this->redirectToRoute('app_pins');
@@ -78,6 +78,16 @@ class PinsController extends AbstractController
             'pin' => $pin,
             'form' => $form->createView()
         ]);
+    }
+
+     #[Route('/pins/{id<[0-9]+>}/delete', name: 'app_pins_delete', methods: ['DELETE'])]
+    public function delete(Pin $pin, EntityManagerInterface $em): Response
+    {
+        $em->remove($pin);
+        $em->flush();
+
+        return $this->redirectToRoute('app_pins');
+
     }
 
 }
