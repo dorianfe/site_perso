5a1a86f892483938ec89c85d5afe7eb333f62331
dorian <dorian.fenestre@free.fr>
2022-05-09 11:31:19 +0200

    added imageFile to formBuilder

--- src/Form/PinType.php
+++ src/Form/PinType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\Pin;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Vich\UploaderBundle\Form\Type\VichImageType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class PinType extends AbstractType
@@ -14,6 +15,16 @@ class PinType extends AbstractType
         $builder
             ->add('title')
             ->add('description')
+            ->add('imageFile', VichImageType::class, [
+            'label' => 'Image au format JPG ou PNG',
+            'required' => false,
+            'allow_delete' => true,
+            'delete_label' => '...',
+            'download_label' => '...',
+            'download_uri' => true,
+            'image_uri' => true,
+            'asset_helper' => true,
+        ])
         ;
     }
 
